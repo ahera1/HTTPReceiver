b5a8b7bfc21a3a97da8a82de5160595cd949c68a
ahera <world.meets.worlds+github@gmail.com>
2023-10-12 20:12:22 +0900

    プロジェクト ファイルを追加します。

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HTTPReceiver.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+            string query = Request.QueryString.ToString();
+
+            string log_query = $"Request.QueryString: {query}";
+            _logger.Log(LogLevel.Information, log_query);
+
+            var stream = Request.Body;
+            var reader = new StreamReader(stream);
+            var body = reader.ReadToEndAsync().Result;
+
+            string log_body = $"Request.Body: {body}";
+            _logger.Log(LogLevel.Information, log_body);
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+builder.Services.AddHttpLogging(options =>
+{
+    options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.RequestPropertiesAndHeaders | Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.ResponseStatusCode;
+});
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.Use(async (context, next) =>
+{
+    context.Request.EnableBuffering();
+    await next();
+});
+
+app.MapRazorPages();
+
+app.Run();
